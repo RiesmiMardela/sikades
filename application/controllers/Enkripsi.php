b97020aab1bfe0eb75f47871d7e6e752fda47719
RiesmiMardela <riesmimardela26@gmail.com>
2022-06-17 21:52:15 +0700

    s

--- application/controllers/Enkripsi.php
+++ application/controllers/Enkripsi.php
@@ -101,7 +101,7 @@ class Enkripsi extends CI_Controller
 
                                 $this->Enkripsi_model->tambahDataEnkripsi($data);
 
-                                redirect('dekripsi');
+                                redirect('dekripsi', 'refresh');
                             } else {
                                 echo "Gagal write file";
                             }

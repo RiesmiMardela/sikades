a415133ac7a694a0958538efdd9e6a0778746af9
RiesmiMardela <riesmimardela26@gmail.com>
2022-06-24 14:35:28 +0700

    a

--- application/views/Dekripsi/downloadPdf.php
+++ application/views/Dekripsi/downloadPdf.php
@@ -1,14 +1,2 @@
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-</head>
-
-<body>
     <?= $plaintext ?>
-
-</body>
-
-</html>

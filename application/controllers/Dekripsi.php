2a7dddafe60d6f0273bc724fe77daec3d6a28dba
RiesmiMardela <riesmimardela26@gmail.com>
2022-06-24 14:46:06 +0700

    a

--- application/controllers/Dekripsi.php
+++ application/controllers/Dekripsi.php
@@ -112,12 +112,12 @@ class Dekripsi extends CI_Controller
 
                 $this->load->library('Pdfgenerator');
                 $dt['plaintext'] = $plaintext;
-                $html = $this->load->view('Dekripsi/downloadPdf', $dt);
+                $this->load->view('Dekripsi/downloadPdf', $dt);
 
                 echo $plaintext;
 
-                // $html = ob_get_contents();
-                // ob_end_clean();
+                $html = ob_get_contents();
+                ob_end_clean();
 
                 $pdfgenerator = new Pdfgenerator();
                 $pdfgenerator->generate($plaintext, $data_file['nama_file'], "A4", "landscape", TRUE);

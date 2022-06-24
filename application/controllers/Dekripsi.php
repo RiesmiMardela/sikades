24782281efa0972e20143e248055b867f1460187
RiesmiMardela <riesmimardela26@gmail.com>
2022-06-24 13:34:36 +0700

    a

--- application/controllers/Dekripsi.php
+++ application/controllers/Dekripsi.php
@@ -112,17 +112,17 @@ class Dekripsi extends CI_Controller
 
                 $this->load->library('Pdfgenerator');
                 $dt['plaintext'] = $plaintext;
-                // $this->load->view('Dekripsi/downloadPdf', $dt);
+                $this->load->view('Dekripsi/downloadPdf', $dt);
 
-                // $html = ob_get_contents();
-                // ob_end_clean();
+                $html = ob_get_contents();
+                ob_end_clean();
 
                 $pdfgenerator = new Pdfgenerator();
                 $pdfgenerator->generate("$plaintext", $data_file['nama_file'], "A4", "landscape", TRUE);
+                $pdfgenerator->loadHtml($html);
                 $pdfgenerator->setPaper('A4', 'landscape');
-                // $pdfgenerator->render();
-                // $pdfgenerator->stream($data_file['nama_file'], array('Attachment' => 0));
-                $pdfgenerator->load_view('Dekripsi/downloadPdf', $dt);
+                $pdfgenerator->render();
+                $pdfgenerator->stream($data_file['nama_file'], array('Attachment' => 0));
                 exit();
             } else {
                 // echo "Salah password";

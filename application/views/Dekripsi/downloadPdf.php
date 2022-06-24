f549a48313bc4904c13475c2f42d5d36c476bbf1
RiesmiMardela <riesmimardela26@gmail.com>
2022-06-24 14:34:29 +0700

    a

--- application/views/Dekripsi/downloadPdf.php
+++ application/views/Dekripsi/downloadPdf.php
@@ -4,11 +4,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <style>
-        * {
-            font-family: DejaVu Sans, sans-serif;
-        }
-    </style>
 </head>
 
 <body>
